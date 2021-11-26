9c16bc6decce81a61bb01f3558daaab5d6838bc1
updhull <abdullahi.ismail@student.moringaschool.com>
2021-11-26 17:53:17 +0300

    This will add hover effect on menu images

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-    $()
-})
+    $(".small").mouseover(function(){
+        $("#small").show();
+    });
+    $(".small").mouseout(function(){
+        $("#small").hide();
+    });
+
+    $(".medium").mouseover(function(){
+        $("#medium").show();
+    });
+    $(".medium").mouseout(function(){
+        $("#medium").hide();
+    });
+    
+    $(".large").mouseover(function(){
+        $("#large").show();
+    });
+    $(".large").mouseout(function(){
+        $("#large").hide();
+    });
+    
+});
